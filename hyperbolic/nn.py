bc0b4ec83e338ab733d423ab44b5d803c2bae8dd
gabriel <g.antunes.moreira@gmail.com>
2023-02-22 06:56:21 +0000

    added poincare exp0 class

---
+++ hyperbolic/nn.py
@@ -0,0 +1,29 @@
+"""
+    hyperbolic/nn.py
+    Feb 22 2023
+    Gabriel Moreira
+"""
+
+import torch.nn as nn
+import functional as hf
+
+class PoincareExp0(nn.Module):
+    def __init__(self, 
+                 k: float,
+                 dim: int,
+                 riemannian: bool):
+        super(PoincareExp0, self).__init__()
+       
+        self.k = k
+        self.riemannian = PoincareGradient
+        self.riemannian.k = k
+
+        if riemannian:
+            self.grad_correction = lambda x: self.riemannian.apply(x)
+        else:
+            self.grad_correction = lambda x: x
+
+    def forward(self, u):
+        x = hf.poincare_exp0(u, k=self.k)
+        return self.grad_fix(hf.project2poincare(x), k=self.k)
+
