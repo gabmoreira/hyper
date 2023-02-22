54cb7e17fafcace0455db17ef45252d0d887dbc3
gabriel <g.antunes.moreira@gmail.com>
2023-02-22 07:35:11 +0000

    finished setting up metrics and projections

--- hyperbolic/functional.py
+++ hyperbolic/functional.py
@@ -7,12 +7,14 @@ import numpy as np
 import torch
 from hmath import atanh, acosh
 
+
 def euclidean_dist(x: torch.Tensor, y: torch.Tensor, keepdim: bool = False):
     """
         Euclidean distance
     """
     dist = torch.norm(x - y, p=2, dim=-1, keepdim=keepdim)
     return dist
+
 
 def poincare_dist(x: torch.Tensor, y: torch.Tensor, k: float, keepdim: bool = False):
     """
@@ -22,6 +24,7 @@ def poincare_dist(x: torch.Tensor, y: torch.Tensor, k: float, keepdim: bool = Fa
     dist = atanh(sqrt_c * mobius_add(-x, y, k).norm(dim=-1, p=2, keepdim=keepdim))
     return dist_c * 2 / sqrt_c
 
+
 def lorentz_distance(x: torch.Tensor, y: torch.Tensor, k: float, keepdim: bool = False):
     """
         Lorentz (curvature k < 0) distance
@@ -29,6 +32,34 @@ def lorentz_distance(x: torch.Tensor, y: torch.Tensor, k: float, keepdim: bool =
     inner = lorentz_inner(x,y,keepdim)
     dist = (1.0 / np.sqrt(-k)) * acosh(k * inner)
     return dist
+
+
+def euclidean_cdist(x: torch.Tensor, y: torch.Tensor):
+    """
+        Euclidean pairwise distance matrix
+    """
+    cdist = torch.cdist(x, y, p=2)
+    return cdist
+
+
+def poincare_cdist(x: torch.Tensor, y: torch.Tensor, k: float):
+    """
+        Poincaré pairwise distance matrix
+    """
+    sqrt_k = (-k) ** 0.5
+    cdist =  (2.0/sqrt_k) * atanh(sqrt_k * torch.norm(mobius_addition_batch(-x, y, k), dim=-1))
+
+
+def lorentz_cdist(x: torch.Tensor, y: torch.Tensor, k: float):
+    """
+        Lorentz pairwise distance matrix
+    """ 
+    metric = torch.ones((x.shape[-1]))
+    metric[...,-1] = -1
+    inner = torch.einsum('ik,jk,k->ij',x,y,metric)
+    cdist = (1.0 / np.sqrt(-k)) * acosh(k*inner)
+    return 
+
 
 def lorentz_inner(x: torch.Tensor, y: torch.Tensor, keepdim: bool = False):
     """
@@ -41,6 +72,7 @@ def lorentz_inner(x: torch.Tensor, y: torch.Tensor, keepdim: bool = False):
         inner = inner.unsqueeze(-1)
     return inner
 
+                        
 def poincare_exp0(u : torch.Tensor, k: float):
     """
         Poincaré (curvature k < 0) exponential map @ 0
@@ -50,6 +82,7 @@ def poincare_exp0(u : torch.Tensor, k: float):
     gamma  = tanh(sqrt_k * u_norm) * u / (sqrt_k * u_norm)
     return gamma
 
+                        
 def lorentz_inclusion(x: torch.Tensor, k: float):      
     """
         Hyperboloid (curvature k < 0) inclusion map
@@ -63,17 +96,41 @@ def lorentz_inclusion(x: torch.Tensor, k: float):
     return xz
 
 
-
 def mobius_add(x: torch.Tensor, y: torch.Tensor, k: float):
+    """
+    """
+    xy = (x * y).sum(dim=-1, keepdim=True)
     x2 = x.pow(2).sum(dim=-1, keepdim=True)
     y2 = y.pow(2).sum(dim=-1, keepdim=True)
-    xy = (x * y).sum(dim=-1, keepdim=True)
     num = (1 - 2 * k * xy - k * y2) * x + (1 + k * x2) * y
     denom = 1 - 2 * k * xy - k ** 2 * x2 * y2
     return num / (denom + 1e-5)
 
 
+def mobius_addition_batch(x: torch.Tensor, y: torch.Tensor, k: float):
+    """
+    """
+    xy = torch.einsum("ij,kj->ik", x, y) # B x C
+    x2 = x.pow(2).sum(-1, keepdim=True)  # B x 1
+    y2 = y.pow(2).sum(-1, keepdim=True)  # C x 1
+    num = 1 - 2 * k * xy - k * y2.permute(1, 0)  # B x C
+    num = num.unsqueeze(2) * x.unsqueeze(1)
+    num = num + (1 + k * x2).unsqueeze(2) * y  # B x C x D
+    denom_part1 = 1 - 2 * k * xy  # B x C
+    denom_part2 = (-k) ** 2 * x2 * y2.permute(1, 0)
+    denom = denom_part1 + denom_part2
+    res = num / (denom.unsqueeze(2) + 1e-5)
+    return res
+                        
 
+def project2poincare(x: torch.Tensor, k: float, eps: float=1e-3):
+    norm = torch.clamp_min(x.norm(dim=-1, keepdim=True, p=2), 1e-5)
+    maxnorm = (1.0 - eps) / ((-k) ** 0.5)
+    cond = norm > maxnorm
+    projected = x / norm * maxnorm
+    return torch.where(cond, projected, x)
+                        
+                        
 def mobius_transform(p, k):
     """
         Returns an hyperbolic isometry that sends p to the origin
