49671a7b3a4ecedcefb08fb9b6d80ca314746529
gabriel <g.antunes.moreira@gmail.com>
2023-02-24 08:00:46 +0000

    mistake in metric/manifold cfg

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
            'manifold_k'       : 0.0501,
            'metric'           : 'poincare',
            'metric_k'         : -0.05,
-           'n'                : 2}
+           'n'                : 0}
 
     init_experiment(cfg)
     
@@ -97,8 +97,8 @@ if __name__ == '__main__':
     criterion = ProtoLoss(shot=cfg['shot'],
                           way=cfg['way'],
                           query=cfg['query'],
-                          distance_fn=hf.cdist(cfg['manifold'], cfg['metric_k']),
-                          centroid_fn=hf.mean(cfg['manifold'], cfg['metric_k']))
+                          distance_fn=hf.cdist(cfg['metric'], cfg['metric_k']),
+                          centroid_fn=hf.mean(cfg['metric'], cfg['metric_k']))
     
     trainer = Trainer(model,
                       cfg['epochs'],
