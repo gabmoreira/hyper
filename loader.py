a0f61d13719a951a09e6e4328339480cb00a87c6
gabriel <g.antunes.moreira@gmail.com>
2023-03-02 22:01:51 +0000

    merged dataset classes into a single one

--- loader.py
+++ loader.py
@@ -78,11 +78,12 @@ def get_df_transforms(split: str, size=224):
 
 
 
-class CUBData(Dataset):
+class ImSamples(Dataset):
     def __init__(self,
                  img_path: str,
                  data_dict_path: str,
                  im_padding: bool,
+                 target: list[str],
                  transforms):
  
         self.img_path   = img_path
@@ -104,7 +105,7 @@ class CUBData(Dataset):
         for key, value in data_dict.items():
             self.data[key] = value
             
-        self.build_target('target', ['class'])
+        self.build_target('target', target)
         self.build_vocs(['target'])
         
         self.length = len(self.data['target'])
@@ -194,116 +195,3 @@ class CUBData(Dataset):
         s += "Metadata available per datapoint: "
         s += ', '.join(self.data.keys())
         print(s)
-    
-            
-    
-class DeepFashionData(Dataset):
-    def __init__(self,
-                 img_path: str,
-                 data_dict_path: str,
-                 im_padding: bool,
-                 transforms):
- 
-        self.img_path   = img_path
-        self.transforms = transforms
-        self.im_padding = im_padding
-        """
-            Reads dictionary with all data from data_dict_path
-
-            Dict-Keys: 
-                'path', 'gender', 'cat', 'id', 'filename', 'bbox', 'pose'
-
-            Examples:
-                dict['path']     = ['img/WOMEN/Blouses_Shirts/id_00000001/02_1_front.jpg', ...]    
-                dict['gender']   = ['WOMEN', 'WOMEN', ... ]
-                dict['cat']      = ['Blouses_Shirts', 'Blouses_Shirts', 'Tees_Tanks', ...]
-                dict['id']       = ['id_00000001', 'id_00000001', 'id_00000007', ...]
-                dict['filename'] = ['02_1_front.jpg', '02_3_back.jpg', ...]
-                dict['bbox']     = [[50, 40, 140, 180], [10, 55, 78, 180], ...]
-                dict['pose']     = [1, 1, 2, 3, 1, ...]
-        """
-        data_dict = torch.load(data_dict_path)
-        
-        self.data = {}
-        for key, value in data_dict.items():
-            self.data[key] = value
-            
-        self.build_target('target', ['gender', 'cat'])
-        self.build_vocs(['target'])
-        
-        self.length = len(self.data['target'])
-        
-        
-    def build_vocs(self, items: list):
-        """
-            Creates a dictionary of vocabularies for the items requested
-        """
-        self.voc = {item : Vocabulary(self.data[item]) for item in items}
-
-        
-    def build_target(self, target_name: str, items: list):
-        n = len(self.data[items[0]])
-        self.data[target_name] = ['/'.join([str(self.data[t][i]) for t in items]) for i in range(n)]
-           
-            
-    def collate_fn(self, batch):
-        """
-            Create batch tensors from argument batch (list)
-        """
-        batch_dict = {}
-        
-        batch_dict['data']   = torch.cat([self.transforms(b[0]).unsqueeze(0) for b in batch])
-        batch_dict['target'] = torch.tensor([self.voc['target'].w2i(b[1]) for b in batch])
-
-        return batch_dict
-        
-        
-    def __getitem__(self, i):
-        """
-        """ 
-        im = self.process_im(i)
-        target = self.data['target'][i]
-        
-        return im, target
-    
-    
-    def __len__(self):
-        return self.length
-    
-    
-    def pad_im(self, im):
-        """
-            Resizes PIL image as square image with padding
-            Keeps aspect ratio
-            Returns a PIL image
-        """
-        old_size = torch.tensor(im.size[::-1])
-        d = torch.argmax(old_size)
-        
-        new_size = old_size.max()
-
-        pad          = new_size - old_size[1-d]
-        padding      = [0,0,0,0]
-        padding[d]   = pad // 2
-        padding[d+2] = new_size - old_size[1-d] - padding[d]
-
-        new_im = fn.pad(im, padding, fill=255) 
-        
-        return new_im
-    
-    
-    def process_im(self, i, min_bbox_width=100, min_bbox_height=100):
-        """
-            Open, crop, resize i-th image
-        """
-        im = Image.open(os.path.join(self.img_path, self.data['path'][i]))
-        im = im.convert(mode='RGB')
-        
-        bbox = self.data['bbox'][i]
-        im   = im.crop(bbox)
-        
-        if self.im_padding:
-            im = self.pad_im(im)    
-    
-        return im
-    
