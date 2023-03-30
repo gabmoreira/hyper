aeb630630387e71dfe597a575fa6e244c8e9e8f9
gabriel <g.antunes.moreira@gmail.com>
2023-03-30 17:58:01 +0000

    updated type hints

--- hyperbolic/delta.py
+++ hyperbolic/delta.py
@@ -1,11 +1,17 @@
+"""
+    hyperbolic/functional.py
+    Mar 30 2023
+    Gabriel Moreira
+"""
 import torch
 import numpy as np
 from tqdm.auto import tqdm 
 
+from typing import Callable
 
-def delta_hyp(cdist):
+def delta_hyp(cdist: torch.Tensor):
     """
-    computes delta hyperbolicity value from distance matrix
+        Computes delta hyperbolicity value from distance matrix
     """
 
     p = 0
@@ -18,11 +24,16 @@ def delta_hyp(cdist):
     return delta
 
 
-def delta_hyperbolicity(x, distance_fn, maxiter=10, batch_size=1500):
+def delta_hyperbolicity(x : torch.Tensor,
+                        distance_fn: Callable,
+                        maxiter : int=10,
+                        batch_size : int =1500):
+    """
+        Computes delta hyperbolicity in batches from tensor of points
+    """
     vals = []
     for _ in tqdm(range(maxiter)):
         idx = np.random.randint(0, x.shape[0], size=batch_size)
-        
         x_batch = x[idx,...]
         cdist   = distance_fn(x_batch)
         diam    = cdist.max()
