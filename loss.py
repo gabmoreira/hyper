601246341d23a5ce7ae0d8a82e0ac324d6f9633d
gabriel <g.antunes.moreira@gmail.com>
2023-04-07 21:16:55 +0000

    removed NNAcc loss

--- loss.py
+++ loss.py
@@ -11,12 +11,12 @@ from typing import Callable
 
 class ProtoLoss(nn.Module):
     def __init__(self,
-                 shot: int,
-                 way: int,
-                 query: int,
+                 shot:        int,
+                 way:         int,
+                 query:       int,
                  distance_fn: Callable,
                  centroid_fn: Callable,
-                 device: str='cuda'):
+                 device:      str='cuda'):
         
         super(ProtoLoss, self).__init__()
         self.shot  = shot
@@ -52,52 +52,5 @@ class ProtoLoss(nn.Module):
     
     def scores(self):
         return self.tc, self.t 
-
-    
-class NNAcc:
-    def __init__(self, node_embeddings, voc, metric):
-        """
-        Nearest-Neighbor Classification Accuracy
-        
-        Parameters 
-        ----------
-        node_embeddings - dictionary with full node name as key
-        and node embedding as value. E.g., node_embeddings['WOMEN/Skirts']=torch.tensor(...)
-        voc             - vocabulary dictionary 
-        metric          - torch.tensor  of shape n with hyperbolic metric signature
-        """
-        super(NNAcc, self).__init__()
-        
-        node_labels = list(node_embeddings.keys())
-        
-        self.metric = metric
-        self.voc    = voc
-        
-        self.cat_labels = []
-        for label in node_labels:
-            if len(label.split('/')) == 2:
-                self.cat_labels.append(label)
-        
-        self.cat_embeddings = torch.stack([node_embeddings[label] for label in self.cat_labels], dim=0)
-        
-        
-    def acc(self, features, cat):
-        inner_products = torch.matmul(self.metric * features, self.cat_embeddings.transpose(1,0))
-        distances      = torch.acosh(-inner_products)
-        
-        predictions = torch.argmin(distances, dim=1, keepdim=False)
-        
-        total = 0
-        for i in range(len(predictions)):
-            pred_cat = self.cat_labels[predictions[i]].split('/')[-1]
-            if self.voc['cat']._idx2word[cat[i].item()] == pred_cat:
-                total += 1
-                
-        return total
-    
-    
-    def __call__(self, features, cat):
-        return self.acc(features, cat)
-        
             
             
