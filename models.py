22fa6b4957f64b7b4d3eb7b3038d260d02fc3f0e
gabriel <g.antunes.moreira@gmail.com>
2023-04-24 23:20:08 +0000

    added vic model

--- models.py
+++ models.py
@@ -12,7 +12,37 @@ import hyperbolic.functional as hf
 
 from backbone import Resnet50, Convnet
                         
-                                           
+
+def create_mlp(in_dim: int, mlp_dims: str):
+    """
+        Create MLP with mlp_dims e.g., "128-128-128"
+    """
+    mlp_spec = f"{in_dim}-{mlp_dims}"
+    layers = []
+    
+    f = list(map(int, mlp_spec.split("-")))
+    
+    for i in range(len(f) - 2):
+        layers.append(nn.Linear(f[i], f[i + 1]))
+        layers.append(nn.BatchNorm1d(f[i + 1]))
+        layers.append(nn.ReLU(True))
+    layers.append(nn.Linear(f[-2], f[-1], bias=False))
+    
+    return nn.Sequential(*layers)
+
+
+class VICModel(nn.Module):
+    def __init__(self, backbone: nn.Module, projector: nn.Module):
+        super().__init__()
+        self.backbone  = backbone
+        self.projector = projector
+
+    def forward(self, x: torch.Tensor):
+        x = x.flatten(start_dim=0,end_dim=1)
+        x = self.projector(self.backbone(x))
+        
+        return x
+    
 def manifold_encoder(backbone: str,
                      manifold: str,
                      dim: int,
