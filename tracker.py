001eb0cbfbafdcd5c2d9d1379010f687f2262c34
gabriel <g.antunes.moreira@gmail.com>
2023-04-25 22:21:27 +0000

    tracker more general

--- tracker.py
+++ tracker.py
@@ -14,70 +14,88 @@ from typing import List
 
 class Tracker:
     def __init__(self,
-                 metrics: List[str],
                  filename: str,
                  load: bool=False):
-        '''
-        '''
+
+        """
+        """
         self.filename = os.path.join(filename, 'tracker.csv')
 
         if load:
             self.metrics_dict = self.load()
+            self.init = False
         else:        
             self.metrics_dict = {}
-            for metric in metrics:
-                self.metrics_dict[metric] = []
-
-
-    def update(self, **args):
-        '''
-        '''
-        for metric in args.keys():
-            assert(metric in self.metrics_dict.keys())
-            self.metrics_dict[metric].append(args[metric])
+            self.init = True
+        
+        
+    def update(self, **kwargs):
+        """
+        """
+        if self.init:
+            for metric, value in kwargs.items():
+                self.metrics_dict[metric].append(value)
+            self.init = False
+        else:
+            for metric, value in kwargs.items():
+                assert metric in self.metrics_dict.keys(), 'unknown metric'
+                self.metrics_dict[metric].append(value)
 
         self.save()
 
 
     def is_larger(self, metric: str, value: float):
-        '''
-        '''
-        assert(metric in self.metrics_dict.keys())
+        """
+        """
+        assert metric in self.metrics_dict.keys(), 'unknown metric'
         if len(self.metrics_dict[metric]) > 0:
-            return sorted(self.metrics_dict[metric])[-1] < value
+            return max(self.metrics_dict[metric]) < value
         else:
             return True
 
 
     def is_smaller(self, metric: str, value: float):
-        '''
-        '''
-        assert(metric in self.metrics_dict.keys())
+        """
+        """
+        assert metric in self.metrics_dict.keys(), 'unknown metric'
         if len(self.metrics_dict[metric]) > 0:
-            return sorted(self.metrics_dict[metric])[0] > value
+            return min(self.metrics_dict[metric]) > value
         else:
             return True
 
 
+    def is_better(self, metric: str, value: float):
+        """
+        """
+        assert(metric in self.metrics_dict.keys())
+        if len(self.metrics_dict[metric]) > 0:
+            if metric == 'val_acc':
+                return self.is_larger(metric, value)
+            elif metric == 'val_loss':
+                return self.is_smaller(metric, value)
+        else:
+            return True
+        
+        
     def save(self):
-        '''
-        '''
+        """
+        """
         df = pd.DataFrame.from_dict(self.metrics_dict)
         df = df.set_index('epoch')
         df.to_csv(self.filename)
 
 
     def load(self):
-        '''
-        '''
+        """
+        """
         df = pd.read_csv(self.filename)  
         metrics_dict = df.to_dict(orient='list')
         return metrics_dict
 
 
     def __len__(self):
-        '''
-        '''
+        """
+        """
         return len(self.metrics_dict)
     
     
@@ -113,23 +131,23 @@ def plot_trackers(trackers, cfgs):
     plt.subplot(2,2,1)
     for k, tracker in trackers.items():
         sns.lineplot(data=tracker, y="val_acc", x='epoch', linewidth=0.6)    
-
-    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone'])  for k in cfgs.keys()], fontsize=7)
+    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone']) 
+                       for k in cfgs.keys()], fontsize=7)
 
     plt.subplot(2,2,2)
     for k, tracker in trackers.items():
         sns.lineplot(data=tracker, y="train_acc", x='epoch', linewidth=0.6)    
-
-    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone'])  for k in cfgs.keys()], fontsize=7)
+    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone'])  
+                       for k in cfgs.keys()], fontsize=7)
 
     plt.subplot(2,2,3)
     for k, tracker in trackers.items():
         sns.lineplot(data=tracker, y="val_loss", x='epoch', linewidth=0.6)    
-
-    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone'])  for k in cfgs.keys()], fontsize=7)
+    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone']) 
+                       for k in cfgs.keys()], fontsize=7)
 
     plt.subplot(2,2,4)
     for k, tracker in trackers.items():
         sns.lineplot(data=tracker, y="train_loss", x='epoch', linewidth=0.6)    
-
-    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone'])  for k in cfgs.keys()], fontsize=7)
+    plt.legend(labels=['{}  |  {}'.format(cfgs[k]['name'], cfgs[k]['backbone']) 
+                       for k in cfgs.keys()], fontsize=7)
