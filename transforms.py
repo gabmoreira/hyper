15722828d1df8e1394604616763fa533d551a1d2
gabriel <g.antunes.moreira@gmail.com>
2023-04-25 22:22:17 +0000

    moved image padding transform here

--- transforms.py
+++ transforms.py
@@ -1,10 +1,10 @@
 """
     transforms.py
-    Mar 4 2023
+    Apr 24 2023
     Gabriel Moreira
 """
 import numpy as np
-
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps
 from torchvision.transforms import InterpolationMode
 import torchvision.transforms.functional as fn
 import torchvision.transforms as T
@@ -14,6 +14,31 @@ transformtypedict = dict(Brightness=ImageEnhance.Brightness,
                          Sharpness=ImageEnhance.Sharpness,
                          Color=ImageEnhance.Color)
 
+
+class SquarePadding(object):
+    """
+        Pads image to make it square
+        Keeps aspect ratio
+        Returns a PIL image
+    """
+    def __init__(self, fill=0):
+        self.fill = fill
+    
+    def __call__(self, im)
+        old_size = torch.tensor(im.size[::-1])
+        d = torch.argmax(old_size)
+        
+        new_size = old_size.max()
+
+        pad          = new_size - old_size[1-d]
+        padding      = [0,0,0,0]
+        padding[d]   = pad // 2
+        padding[d+2] = new_size - old_size[1-d] - padding[d]
+
+        new_im = fn.pad(im, padding, fill=self.fill) 
+        
+        return new_im
+    
 
 class GaussianBlur(object):
     def __init__(self, p):
@@ -99,13 +124,15 @@ class ImageJitter(object):
 def get_cub_transforms(split: str=None, size: int=84):
     """
     """
-    t_train = T.Compose([T.RandomResizedCrop(size),
+    t_train = T.Compose([SquarePadding(),
+                         T.RandomResizedCrop(size),
                          ImageJitter(dict(Brightness=0.4, Contrast=0.4, Color=0.4)),
                          T.RandomHorizontalFlip(),
                          T.ToTensor(),
                          T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
         
-    t_val = T.Compose([T.Resize(size, interpolation=T.InterpolationMode.BICUBIC),
+    t_val = T.Compose([SquarePadding(),
+                       T.Resize(size, interpolation=T.InterpolationMode.BICUBIC),
                        T.CenterCrop(size),
                        T.ToTensor(),
                        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
@@ -116,27 +143,3 @@ def get_cub_transforms(split: str=None, size: int=84):
         return t_val
     else:
         return t_train, t_val
-    
-    
-def get_df_transforms(split: str, size: int=224):
-    """
-    """
-    t_train = T.Compose([T.RandomResizedCrop(size, scale=(0.2, 1.)),
-                         T.RandomHorizontalFlip(),
-                         T.RandomApply([T.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-                         T.RandomGrayscale(p=0.2),
-                         T.ToTensor(),
-                         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    
-    t_val = T.Compose([T.Resize(size, interpolation=T.InterpolationMode.BICUBIC),
-                       T.ToTensor(),
-                       T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-
-    if split == 'train':
-        return t_train
-    elif split == 'test' or split == 'val':
-        return t_val
-    else:
-        return t_train, t_val
-
-
