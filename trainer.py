e1328ba036c139098952cf587580e58449f815fa
gabriel <g.antunes.moreira@gmail.com>
2023-06-27 05:39:02 +0100

    updated

--- trainer.py
+++ trainer.py
@@ -44,6 +44,7 @@ class Trainer:
         self.best_on      = best_on
         
         self.tracker = Tracker(os.path.join('./experiments', name), load=resume)
+        self.scaler  = torch.cuda.amp.GradScaler()
 
         if resume:
             self.resume_checkpoint()
@@ -53,18 +54,19 @@ class Trainer:
         """
             Fit model to training set over #epochs
         """
-        is_best = False
-
+        is_best = False        
         for epoch in range(self.start_epoch, self.epochs+1):
             train_out = self.train_epoch(epoch)
-            
-            if epoch % val_freq == 0:
+
+            if epoch == 1 or epoch % self.val_freq == 0:
                 val_out = self.validate_epoch()
-            
-            self.epoch_verbose(epoch, **train_out, **val_out)
+                self.epoch_verbose(epoch, **train_out, **val_out)
                 
-            # Check if better than previous models
-            is_best = self.tracker.is_better(best_on, eval(best_on))
+                is_best = self.tracker.is_better(self.best_on, val_out[self.best_on])
+                if is_best:
+                    self.save_best()
+            
+            self.save_checkpoint(epoch)
 
             self.tracker.update(epoch=epoch,
                                 lr=self.optimizer.param_groups[0]['lr'],
@@ -72,7 +74,6 @@ class Trainer:
                                 **val_out)
 
             self.scheduler.step()
-            self.save_checkpoint(epoch, is_best)
             
 
     def train_epoch(self, epoch):
@@ -85,14 +86,18 @@ class Trainer:
         
         out = {}
         for i_batch, batch_dict in enumerate(self.train_loader):
-            self.optimizer.zero_grad()
             batch_data = batch_dict['data'].to(self.device)
-            batch_features = self.model(batch_data)
+
+            self.optimizer.zero_grad()            
+            with torch.cuda.amp.autocast():
+                batch_features = self.model(batch_data)
             
             if 'target' in batch_dict:
                 batch_target = batch_dict['target'].to(self.device)
-                loss_batch = self.train_loss(batch_features, batch_target)
+                loss_batch   = self.train_loss(batch_features, batch_target)
+                
                 total_loss += loss_batch.detach() 
+                
                 out['train_loss'] = float(total_loss / len(self.train_loader))
 
                 tc, t = self.train_loss.scores()
@@ -108,17 +113,20 @@ class Trainer:
             
             else:
                 loss_batch = self.train_loss(batch_features)
+                
                 total_loss += loss_batch.detach() 
+                
                 out['train_loss'] = float(total_loss / len(self.train_loader))
 
                 batch_bar.set_postfix(
-                    loss="{:1.5e}".format(out['train_loss']),
+                    loss="{:1.5e}".format(total_loss / (i_batch + 1)),
                     lr="{:1.2e}".format(float(self.optimizer.param_groups[0]['lr'])))
                 batch_bar.update()
                 
-            loss_batch.backward()
-            self.optimizer.step()
-
+            self.scaler.scale(loss_batch).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            
         batch_bar.close()
 
         return out
@@ -148,26 +156,28 @@ class Trainer:
 
             total_loss += loss_batch.detach()
             
-        out['val_loss'] = float(total_loss / len(self.val_loader)),
+        out['val_loss'] = float(total_loss / len(self.val_loader))
         return out
 
 
-    def save_checkpoint(self, epoch, is_best):
+    def save_checkpoint(self, epoch):
         checkpoint = {"epoch"     : epoch,
                       "model"     : self.model.state_dict(),
                       "optimizer" : self.optimizer,
-                      "scheduler" : self.scheduler }
+                      "scheduler" : self.scheduler,
+                      "scaler"    : self.scaler}
 
         # Save checkpoint to resume training later
         checkpoint_path = os.path.join('./experiments', self.name, 'checkpoint.pt')
         torch.save(checkpoint, checkpoint_path)
         print('Checkpoint saved: {}'.format(checkpoint_path))
 
+
+    def save_best(self):
         # Save best model weights
-        if is_best:
-            best_path = os.path.join('./experiments/', self.name, "best_weights.pt")
-            torch.save(self.model.state_dict(), best_path)
-            print("Saving best model: {}".format(best_path))
+        best_path = os.path.join('./experiments/', self.name, "best_weights.pt")
+        torch.save(self.model.state_dict(), best_path)
+        print("Saving best model: {}".format(best_path))
 
 
     def resume_checkpoint(self):
@@ -178,6 +188,7 @@ class Trainer:
         self.start_epoch = checkpoint['epoch'] + 1
         self.optimizer   = checkpoint['optimizer']
         self.scheduler   = checkpoint['scheduler']
+        self.scaler      = checkpoint['scaler']
         self.model.load_state_dict(checkpoint["model"])
         
         print('Checkpoint loaded. Resume training from epoch {}'.format(self.start_epoch))
